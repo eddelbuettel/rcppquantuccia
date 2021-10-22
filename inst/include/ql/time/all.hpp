8ff8a7899867aa21c1fbf79fe3fbd0ccc39c7f80
Dirk Eddelbuettel <edd@debian.org>
2021-10-21 20:06:49 -0500

    added ecb

--- inst/include/ql/time/all.hpp
+++ inst/include/ql/time/all.hpp
@@ -3,11 +3,14 @@
 #include <ql/time/calendar.hpp>
 #include <ql/time/date.hpp>
 #include <ql/time/dategenerationrule.hpp>
-//#include <ql/time/ecb.hpp>
+// -- not needed  #include <ql/time/daycounter.hpp>
+#include <ql/time/ecb.hpp>
 #include <ql/time/frequency.hpp>
 #include <ql/time/imm.hpp>
 #include <ql/time/period.hpp>
 #include <ql/time/schedule.hpp>
 #include <ql/time/timeunit.hpp>
 #include <ql/time/weekday.hpp>
+
 #include <ql/time/calendars/all.hpp>
+// -- not needed  #include <ql/time/daycounters/all.hpp>
