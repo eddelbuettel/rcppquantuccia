b78dc3897f77ed504bb574ec450e5d901f513d83
Dirk Eddelbuettel <edd@debian.org>
2021-10-29 16:49:57 -0500

    modified demo

--- local/calDemo.R
+++ local/calDemo.R
@@ -1,7 +1,16 @@
 
 library(RcppQuantuccia)
 library(data.table)
-nHol <- function(cal) { setCalendar(cal); length(getHolidays(as.Date("2022-01-01"), as.Date("2022-12-31"))) }
+
+calendars
+
+getHols <- function(cal) {    # simple help function
+    setCalendar(cal)
+    getHolidays(as.Date("2022-01-01"), as.Date("2022-12-31"))
+}
 D <- data.table(calendar=calendars)
-D[ , holidays := nHol(calendars), by=calendars ]
+D[ , `:=`(n = length(getHols(calendar)),
+          holidays = paste(format(getHols(calendar),"%d %b"), collapse=",")),
+  by = calendar ]
+D
 print(D[order(-holidays)], nrow=length(calendars))
